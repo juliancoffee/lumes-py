a64c3d432d7273a8efc1de8fc9f802a713501297
juliancoffee <lightdarkdaughter@gmail.com>
2019-07-12 02:30:30 +0300

    it's done, just need some cosmetic fix

---
+++ src/lumes.py
@@ -0,0 +1,38 @@
+import subprocess
+from typing import List
+
+def screenshot(args: List[str]) -> bytes:
+    """
+    Take a screenshot and return it as bytes
+    """
+    if args[0] == "-s":
+        img = subprocess.run(["maim", "-s"], stdout=subprocess.PIPE).stdout
+    else:
+        img = subprocess.run(["maim"], stdout=subprocess.PIPE).stdout
+    return img
+
+
+def clip_board(img: bytes) -> None:
+    """
+    Take image from arguments (bytes returned from function screenshot) and place it to clipboard using xclip
+    """
+    clip = subprocess.run(["xclip", "-selection", "clipboard", "-t", "image/png"], input=img)
+
+
+def save_file(img: bytes, path: str) -> None:
+    """
+    Save file (bytes returned from screenshot) to given path
+    """
+    with open(path, mode='wb') as temp:
+        temp.write(img)
+
+def main():
+    default_path = "im.png"
+    args = ["-s"]
+    img = screenshot(args)
+    clip_board(img)
+    save_file(img, default_path)
+
+
+if __name__ == "__main__":
+    main()
--- src/pipe.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-import sys
-
-image = sys.stdin.buffer.read()
-sys.stdout.buffer.write(image)
