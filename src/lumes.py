b172f368540591a252dc833e5da9d938f0102e02
juliancoffee <lightdarkdaughter@gmail.com>
2019-07-12 17:41:20 +0300

    nothing

--- src/lumes.py
+++ src/lumes.py
@@ -1,6 +1,7 @@
 import subprocess
 from typing import List
 
+
 def screenshot(args: List[str]) -> bytes:
     """
     Take a screenshot and return it as bytes
@@ -14,9 +15,14 @@ def screenshot(args: List[str]) -> bytes:
 
 def clip_board(img: bytes) -> None:
     """
-    Take image from arguments (bytes returned from function screenshot) and place it to clipboard using xclip
+    Take image from arguments (bytes returned from function screenshot)
+    and place it to clipboard using xclip
     """
-    clip = subprocess.run(["xclip", "-selection", "clipboard", "-t", "image/png"], input=img)
+    clip = subprocess.run(
+                ["xclip",
+                    "-selection", "clipboard",
+                    "-t", "image/png"],
+                input=img)
 
 
 def save_file(img: bytes, path: str) -> None:
@@ -25,6 +31,7 @@ def save_file(img: bytes, path: str) -> None:
     """
     with open(path, mode='wb') as temp:
         temp.write(img)
+
 
 def main():
     default_path = "im.png"
